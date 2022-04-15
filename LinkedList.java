757a762fd7a631fadb897e415ee701a7a2608127
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-04-15 13:50:26 +0530

    Linked List Progrma

--- LinkedList.java
+++ LinkedList.java
@@ -11,4 +11,17 @@ public class LinkedList<T>{
 		current = null;
 		position = -1;
 }
+
+	public void add(T data){
+		Node<T> node = new Node<T>(data);
+		if(head == null){
+			head = node;
+			current = head;
+		}
+		else{
+			current.next = node;
+			current = current.next;
+		}
+		position++;
+	}
 }
